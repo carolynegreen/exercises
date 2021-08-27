27cffed7979a6a928c66b7e26e82fda401566afe
carolynegreen <carolynemarie94@gmail.com>
2021-08-27 10:42:14 +0200

    truthy falsy

---
+++ basics.js
@@ -0,0 +1,24 @@
+"use strict";
+const bool = true;
+const num = 41;
+const str = "Peter";
+const obj = {
+  cats: 2,
+  cars: 1,
+};
+const nothing = null;
+let undf;
+const symbol = Symbol("symbol");
+console.log();
+
+const n1 = 1;
+const n2 = 2;
+const s1 = "1";
+const s2 = "2";
+
+const value = NaN;
+if (value) {
+  console.log("Value is truthy");
+} else {
+  console.log("Value is falsy");
+}

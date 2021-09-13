2672fd7417ec3efbde162dfd874570f007905c33
carolynegreen <carolynemarie94@gmail.com>
2021-09-13 16:33:11 +0200

    saving stuff

--- test2.js
+++ test2.js
@@ -18,3 +18,33 @@ function testParamsMakingErrors(a, b, c, d) {
   console.log(`${a} and ${c[b + 1]} are enemies`);
 }
 people.forEach(testParamsMakingErrors);
+
+const animals = [
+  { name: "mandu", type: "cat" },
+  { name: "mia", type: "cat" },
+  { name: "doug", type: "dog" },
+];
+
+function isCat(animal) {
+  if (animal.type === "cat") {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log("all animals");
+console.log(animals);
+
+console.log("starts with p");
+//const cat = animals.filter(isCat);
+console.log(startsWithP);
+
+console.log("dogs");
+console.log();
+
+const startsWithP = animals.filter(function (animals) {
+  if (animals.name.charAt(0) === "P") {
+    return true;
+  }
+  return false;
+});
